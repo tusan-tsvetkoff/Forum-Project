f3fc69f1bcf2f713049c5c4f01c997158bcd7cd9
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-06-09 14:00:26 +0300

    Setting up..

---
+++ Forum/Forum.Contracts/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Forum.Contracts;
+public class Class1
+{
+
+}
---
+++ Forum/Forum.Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Forum.Models;
+public class User
+{
+    public Guid Id { get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public string Username {get; set;}
+    public string Email {get; set;}
+}
+

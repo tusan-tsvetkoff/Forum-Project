baa03f240d63dc30927b9da01b397546f9585316
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-06-10 06:02:42 +0300

    In case I missed something

---
+++ Forum/Forum.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Forum.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Forum.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            return services;
+        }
+    }
+}
---
+++ Forum/Forum.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Forum.Infrastructure;
+public class Class1
+{
+
+}
